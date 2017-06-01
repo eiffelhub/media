a8b82b9dfa8cb41236532e3f67be8a21f969e425
Jocelyn Fiat <github@djoce.net>
2017-06-01 23:18:11 +0200

    Updated tool.

--- logo/bin/svg_to.bat
+++ logo/bin/svg_to.bat
@@ -4,8 +4,19 @@ setlocal
 call %~dp0etc.cmd
 
 set TMPBASENAME=%~n1
+if exist "%2_%3x%4\%TMPBASENAME%.%2" goto skipped
+goto export
+
+
+:skipped
+@echo Skipped %TMPBASENAME% to %2 (size %3x%4) : already exists!
+goto end
+
+:export
 @echo Convert %TMPBASENAME% to %2 (size %3x%4)
 if not exist %2_%3x%4 mkdir %2_%3x%4
-if not exist "%2_%3x%4\%TMPBASENAME%.%2" %INKSCAPE_CMD% -z --g-fatal-warnings --export-%2="%2_%3x%4\%TMPBASENAME%.%2" --export-width=%3 --export-height=%4 "%TMPBASENAME%.svg" 
+%INKSCAPE_CMD% -z --g-fatal-warnings --export-%2="%2_%3x%4\%TMPBASENAME%.%2" --export-width=%3 --export-height=%4 "%TMPBASENAME%.svg" 
+goto end
 
+:end
 endlocal
